62b5cdd3b5236bc9fa6445667333b7fa936b7d23
vincenzo mezzela <mezzela@eurecom.fr>
2023-03-17 20:11:59 +0100

    added call to target function

--- fuzzers/mc2_fuzzer/harness.c
+++ fuzzers/mc2_fuzzer/harness.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <assert.h>
+#include <stdio.h>
 
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
--- fuzzers/mc2_fuzzer/src/main.rs
+++ fuzzers/mc2_fuzzer/src/main.rs
@@ -1,3 +1,9 @@
+use libafl_targets::libfuzzer_test_one_input;
+
 fn main() {
+    let bytes: [u8; 1] = [b'a'];
+
+    libfuzzer_test_one_input(&bytes);
+
     println!("Hello, world!");
 }
