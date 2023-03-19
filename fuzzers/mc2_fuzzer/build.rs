44aeda933f7573dbd9518872463a256da4377a57
vincenzo mezzela <mezzela@eurecom.fr>
2023-03-19 20:09:10 +0100

    updated building of the instrumented target program

--- fuzzers/mc2_fuzzer/build.rs
+++ fuzzers/mc2_fuzzer/build.rs
@@ -1,26 +1,20 @@
 // build.rs
 
-use std::env;
+use std::process::Command;
 
-// clang-11 -c harness.c -g3 -O0 -o harness.o
-// clang-11 -Xclang -load -Xclang build/skeleton/libSkeletonPass.so -g3 -O0 onebyte_test.c runtime_func.o -o onebyte_test -lm
 
 fn main() {
-    let out_dir = env::var_os("OUT_DIR").unwrap();
-    let out_dir = out_dir.to_string_lossy().to_string();
 
-    println!("cargo:rerun-if-changed=harness.c");
+    Command::new("make")
+            .arg("clean")
+            .current_dir("./harness")
+            .status()
+            .expect("failed to execute make clean");
 
-    std::env::set_var("CC", "clang");
-    std::env::set_var("CXX", "clang++");
+    Command::new("make")
+            .arg("build")
+            .current_dir("./harness")
+            .status()
+            .expect("failed to execute make build");
 
-    cc::Build::new()
-        .flag("-g3")
-        .flag("-O0")
-        .file("harness.c")
-        .compile("harness");
-
-    println!("cargo:rustc-link-search=native={}", &out_dir);
-
-    println!("cargo:rerun-if-changed=build.rs");
 }
