f97446e12e8e72d972caf46e866b717f7ae95f1c
vincenzo mezzela <mezzela@eurecom.fr>
2023-03-21 16:33:16 +0100

    updated building files

--- fuzzers/mc2_fuzzer/build.rs
+++ fuzzers/mc2_fuzzer/build.rs
@@ -2,8 +2,12 @@
 
 use std::process::Command;
 
+// clang-11 -Xclang -load -Xclang build/skeleton/libSkeletonPass.so -g3 -c -O0 harness.c -o harness.o 
+
 
 fn main() {
+
+    std::env::set_var("CC", "clang-11");
 
     Command::new("make")
             .arg("clean")
@@ -17,4 +21,13 @@ fn main() {
             .status()
             .expect("failed to execute make build");
 
+	cc::Build::new()
+			.flag("-Xclang")
+			.flag("-load")
+			.flag("-Xclang")
+			.flag("harness/build/skeleton/libSkeletonPass.so")
+			.file("./harness/harness.c")
+			.flag("-g3")
+			.flag("-O0")
+			.compile("harness")
 }
--- fuzzers/mc2_fuzzer/src/main.rs
+++ fuzzers/mc2_fuzzer/src/main.rs
@@ -1,39 +1,11 @@
-extern crate libloading;
-
-use libloading::{Library, Symbol};
-use libc::{c_int, size_t};
-
-type LLVMFuzzerTestOneInputFn = extern "C" fn(data: *const u8, size: size_t) -> c_int;
-
-// #[no_mangle]
-// pub extern "C" fn log_func32(br_id: u32, old_cond: bool, arg1: u32, arg2: u32,
-//                             is_signed: u8, cond_type: u8) -> bool {
-//     println!("br_id: {}, old_cond: {}, arg1: {}, arg2: {}, is_signed: {}, cond_type: {}",
-//                                 br_id, old_cond, arg1, arg2, is_signed, cond_type);
-//     old_cond
-// }
-
-
-
-/*
-    The code is broken, we need to define functions for log_func32
-    otherwise the runtime cannot resolve the symbol
- */
+use libafl_targets::{
+    libfuzzer_test_one_input
+};
 
 fn main() {
-    
-    // Load the shared library
-    let lib = Library::new("./harness/harness.so").unwrap();
 
-    // Get a reference to the LLVMFuzzerTestOneInput function.
-    let fuzzer_test_one_input: Symbol<LLVMFuzzerTestOneInputFn> = unsafe {
-        lib.get(b"LLVMFuzzerTestOneInput\0")
-            .unwrap()
-    };
-    
-    // Call the LLVMFuzzerTestOneInput function with some data.
-    let data = b"input data";
-    let result = fuzzer_test_one_input(data.as_ptr(), data.len());
+    let input = b"a";
+    libfuzzer_test_one_input(input);
 
-    println!("LLVMFuzzerTestOneInput returned: {}", result);
+    println!("Hello");
 }
