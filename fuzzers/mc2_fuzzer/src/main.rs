07591dc0fc6dc9f36f34116c7d22300d5d6e566c
Marco Cavenati s294889 <s294889@studenti.polito.it>
2023-03-28 22:13:53 +0200

    Refactoring

--- fuzzers/mc2_fuzzer/src/main.rs
+++ fuzzers/mc2_fuzzer/src/main.rs
@@ -1,4 +1,4 @@
-const maxpossibleBBs: u32 = 4000;
+const MAXPOSSIBLE_BBS: u32 = 4000;
 
 extern "C" {
     fn LLVMFuzzerTestOneInput(data: *const u8, size: usize) -> isize;
@@ -15,56 +15,57 @@ struct BranchCmp {
 
 enum Predicate {
     /// 0 0 0 0    Always false (always folded)
-    FCMP_FALSE = 0,
+    FcmpFalse = 0,
     /// 0 0 0 1    True if ordered and equal
-    FCMP_OEQ = 1,
+    FcmpOeq = 1,
     /// 0 0 1 0    True if ordered and greater than
-    FCMP_OGT = 2,
+    FcmpOgt = 2,
     /// 0 0 1 1    True if ordered and greater than or equal
-    FCMP_OGE = 3,
+    FcmpOge = 3,
     /// 0 1 0 0    True if ordered and less than
-    FCMP_OLT = 4,
+    FcmpOlt = 4,
     /// 0 1 0 1    True if ordered and less than or equal
-    FCMP_OLE = 5,
+    FcmpOle = 5,
     /// 0 1 1 0    True if ordered and operands are unequal
-    FCMP_ONE = 6,
+    FcmpOne = 6,
     /// 0 1 1 1    True if ordered (no nans)
-    FCMP_ORD = 7,
+    FcmpOrd = 7,
     /// 1 0 0 0    True if unordered: isnan(X) | isnan(Y)
-    FCMP_UNO = 8,
+    FcmpUno = 8,
     /// 1 0 0 1    True if unordered or equal
-    FCMP_UEQ = 9,
+    FcmpUeq = 9,
     /// 1 0 1 0    True if unordered or greater than
-    FCMP_UGT = 10,
+    FcmpUgt = 10,
     /// 1 0 1 1    True if unordered, greater than, or equal
-    FCMP_UGE = 11,
+    FcmpUge = 11,
     /// 1 1 0 0    True if unordered or less than
-    FCMP_ULT = 12,
+    FcmpUlt = 12,
     /// 1 1 0 1    True if unordered, less than, or equal
-    FCMP_ULE = 13,
+    FcmpUle = 13,
     /// 1 1 1 0    True if unordered or not equal
-    FCMP_UNE = 14,
+    FcmpUne = 14,
     /// 1 1 1 1    Always true (always folded)
-    FCMP_TRUE = 15, //< equal
-    ICMP_EQ = 32,
+    FcmpTrue = 15,
+    /// equal
+    IcmpEq = 32,
     /// not equal
-    ICMP_NE = 33,
+    IcmpNe = 33,
     /// unsigned greater than
-    ICMP_UGT = 34,
+    IcmpUgt = 34,
     /// unsigned greater or equal
-    ICMP_UGE = 35,
+    IcmpUge = 35,
     /// unsigned less than
-    ICMP_ULT = 36,
+    IcmpUlt = 36,
     /// unsigned less or equal
-    ICMP_ULE = 37,
+    IcmpUle = 37,
     /// signed greater than
-    ICMP_SGT = 38,
+    IcmpSgt = 38,
     /// signed greater or equal
-    ICMP_SGE = 39,
+    IcmpSge = 39,
     /// signed less than
-    ICMP_SLT = 40,
+    IcmpSlt = 40,
     /// signed less or equal
-    ICMP_SLE = 41,
+    IcmpSle = 41,
 }
 
 fn main() {
@@ -157,7 +158,7 @@ fn log_funchelper(
     is_signed: u8,
     cond_type: u8,
 ) -> bool {
-    assert!(br_id < maxpossibleBBs);
+    assert!(br_id < MAXPOSSIBLE_BBS);
 
     false
 }
