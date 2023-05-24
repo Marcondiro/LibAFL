c94a076901edaf22cffeef1f84966fd490a19131
vincenzo mezzela <mezzela@eurecom.fr>
2023-05-24 17:16:51 +0200

    DummyInProcessExecutor update

--- fuzzers/mc2_fuzzer/src/dummy_in_process_executor.rs
+++ fuzzers/mc2_fuzzer/src/dummy_in_process_executor.rs
@@ -1,63 +1,97 @@
-use core::fmt;
-use core::marker::PhantomData;
-use std::fmt::Debug;
+use core::{
+    borrow::BorrowMut,
+    fmt::{self, Debug, Formatter},
+    marker::PhantomData,
+};
 
 use libafl::{
     executors::{Executor, ExitKind, HasObservers},
     feedbacks::Feedback,
-    inputs::Input,
-    observers::ObserversTuple,
+    observers::{ObserversTuple, UsesObservers},
     prelude::{EventFirer, EventRestarter, UsesInput, UsesState},
+    state::HasClientPerfMonitor,
     Error, HasObjective,
 };
 
 /// The inmem executor simply calls a target function, then returns afterwards.
-pub struct DummyInProcessExecutor<'a, H, I, OT, S>
+pub struct DummyInProcessExecutor<H, HB, OT, S>
 where
-    H: FnMut(&I) -> ExitKind,
-    I: Input,
-    S: UsesInput,
+    H: FnMut(&S::Input) -> ExitKind + ?Sized,
+    HB: BorrowMut<H>,
     OT: ObserversTuple<S>,
+    S: UsesInput,
 {
     /// The harness function, being executed for each fuzzing loop execution
-    harness_fn: &'a mut H,
+    harness_fn: HB,
     /// The observers, observing each run
     observers: OT,
-    phantom: PhantomData<(I, S)>,
+    // Crash and timeout hah
+    phantom: PhantomData<(S, *const H)>,
 }
 
-impl<'a, H, OT, S, I> UsesState for DummyInProcessExecutor<'a, H, I, OT, S>
+impl<H, HB, OT, S> Debug for DummyInProcessExecutor<H, HB, OT, S>
 where
-    H: FnMut(&S::Input) -> ExitKind,
+    H: FnMut(&S::Input) -> ExitKind + ?Sized,
+    HB: BorrowMut<H>,
+    OT: ObserversTuple<S>,
+    S: UsesInput,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("DummyInProcessExecutor")
+            .field("harness_fn", &"<fn>")
+            .field("observers", &self.observers)
+            .finish_non_exhaustive()
+    }
+}
+
+impl<H, HB, OT, S> UsesState for DummyInProcessExecutor<H, HB, OT, S>
+where
+    H: ?Sized + FnMut(&S::Input) -> ExitKind,
+    HB: BorrowMut<H>,
     OT: ObserversTuple<S>,
     S: UsesInput,
 {
     type State = S;
 }
 
-impl<'a, EM, H, I, OT, S, Z> Executor<EM, Z> for DummyInProcessExecutor<'a, H, I, OT, S>
+
+impl<H, HB, OT, S> UsesObservers for DummyInProcessExecutor<H, HB, OT, S>
 where
-    H: FnMut(&I) -> ExitKind,
-    I: Input,
+    H: ?Sized + FnMut(&S::Input) -> ExitKind,
+    HB: BorrowMut<H>,
     OT: ObserversTuple<S>,
+    S: UsesInput,
 {
-    #[inline]
+    type Observers = OT;
+}
+
+impl<EM, H, HB, OT, S, Z> Executor<EM, Z> for DummyInProcessExecutor<H, HB, OT, S>
+where
+    H: FnMut(&S::Input) -> ExitKind + ?Sized,
+    HB: BorrowMut<H>,
+    EM: UsesState<State = S>,
+    OT: ObserversTuple<S>,
+    S: UsesInput,
+    Z: UsesState<State = S>,
+{
+
     fn run_target(
         &mut self,
         fuzzer: &mut Z,
-        state: &mut S,
+        state: &mut Self::State,
         mgr: &mut EM,
-        input: &I,
+        input: &Self::Input,
     ) -> Result<ExitKind, Error> {
-        (self.harness_fn)(input)
+        Ok((self.harness_fn.borrow_mut())(input))
     }
 }
 
-impl<'a, H, I, OT, S> HasObservers for DummyInProcessExecutor<'a, H, I, OT, S>
+impl<H, HB, OT, S> HasObservers for DummyInProcessExecutor<H, HB, OT, S>
 where
-    H: FnMut(&I) -> ExitKind,
-    I: Input,
+    H: FnMut(&S::Input) -> ExitKind + ?Sized,
+    HB: BorrowMut<H>,
     OT: ObserversTuple<S>,
+    S: UsesInput,
 {
     #[inline]
     fn observers(&self) -> &OT {
@@ -70,29 +104,31 @@ where
     }
 }
 
-impl<'a, H, I, OT, S> DummyInProcessExecutor<'a, H, I, OT, S>
+impl<H, HB, OT, S> DummyInProcessExecutor<H, HB, OT, S>
 where
-    H: FnMut(&I) -> ExitKind,
-    I: Input,
+    H: FnMut(&<S as UsesInput>::Input) -> ExitKind + ?Sized,
+    HB: BorrowMut<H>,
     OT: ObserversTuple<S>,
+    S : UsesState + HasClientPerfMonitor,
 {
     /// Create a new in mem executor.
     /// Caution: crash and restart in one of them will lead to odd behavior if multiple are used,
     /// depending on different corpus or state.
-    /// * `harness_fn` - the harness, executiong the function
+    /// * `harness_fn` - the harness, executing the function
     /// * `observers` - the observers observing the target during execution
     /// This may return an error on unix, if signal handler setup fails
-    pub fn new<EM, OC, OF, Z>(
-        harness_fn: &'a mut H,
+    pub fn new<EM, OF, Z>(
+        harness_fn: HB,
         observers: OT,
         _fuzzer: &mut Z,
         _state: &mut S,
         _event_mgr: &mut EM,
     ) -> Result<Self, Error>
     where
-        EM: EventFirer + EventRestarter,
+        Self: Executor<EM, Z, State = S>,
+        EM: EventFirer<State = S> + EventRestarter,
         OF: Feedback<S>,
-        Z: HasObjective,
+        Z: HasObjective<Objective = OF, State = S>, // TODO : should we keep it?
     {
         Ok(Self {
             harness_fn,
@@ -104,21 +140,13 @@ where
     /// Retrieve the harness function.
     #[inline]
     pub fn harness(&self) -> &H {
-        self.harness_fn
+        self.harness_fn.borrow()
     }
 
     /// Retrieve the harness function for a mutable reference.
     #[inline]
     pub fn harness_mut(&mut self) -> &mut H {
-        self.harness_fn
+        self.harness_fn.borrow_mut()
     }
-}
 
-impl<'a, H, I, OT, S> Debug for DummyInProcessExecutor<'a, H, I, OT, S> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("DummyInProcessExecutor")
-            .field("harness_fn", &"<fn>")
-            .field("observers", &self.observers)
-            .finish_non_exhaustive()
-    }
 }
