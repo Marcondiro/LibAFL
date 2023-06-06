50f89f7a7b00180ac570c17b4b507b73ea1a3e14
vincenzo mezzela <mezzela@eurecom.fr>
2023-06-06 10:39:08 +0200

    back to old version of build.rs

--- fuzzers/mc2_fuzzer/build.rs
+++ fuzzers/mc2_fuzzer/build.rs
@@ -19,37 +19,16 @@ fn main() {
         .status()
         .expect("failed to execute make build");
 
-    Command::new("clang-11")
-        .current_dir("./harness")
-        // .arg("-fexperimental-new-pass-manager")
-        .arg("./split-switches-pass.so")
-        .arg("harness.c")
-        .arg("-emit-llvm")
-        // .arg("-S")
-        .arg("-g3")
-        .arg("-O1")
-        .arg("-funroll-loops")
-        .arg("-c")
-        .arg("-o")
-        .arg("harness.bc")
-        .output()
-        .expect("Failed to process harness.bc with split-switches-pass.so");
-
     cc::Build::new()
         .flag("-Xclang")
         .flag("-load")
         .flag("-Xclang")
-        .flag("harness/split-switches-pass.so")
-        .flag("-Xclang")
-        .flag("-load")
-        .flag("-Xclang")
         .flag("harness/build/skeleton/libSkeletonPass.so")
-        .file("./harness/harness.bc")
-        // .file("./harness/harness.c")
+        .file("./harness/harness.c")
         .flag("-g3")
         .flag("-O0")
         .compile("harness");
-
+    
     println!("cargo:rerun-if-changed=harness/harness.c");
-    println!("cargo:rerun-if-changed=harness/skeleton/Skeleton.cpp");
+    println!("cargo:rerun-if-changed=harness/skeleton/skeleton.cpp");
 }
